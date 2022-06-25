c712ccde10c07e9ed1c1041fbcbc9e26a304bff9
xiezc <765150816@qq.com>
2022-06-25 16:40:06 +0800

    1

--- vxpt-eurake/deploy.sh
+++ vxpt-eurake/deploy.sh
@@ -2,7 +2,7 @@
 docker run --rm -u gradle \
 -v "$PWD/../":/home/gradle/project \
 -w /home/gradle/project/vxpt-eurake \
-gradle gradle assemble
+gradle:7-jdk11 gradle assemble
 
 ## 删除旧镜像
 docker rmi vxpt-eurake:1.0
