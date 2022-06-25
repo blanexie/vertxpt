d526d1f7300bc420a4fd6bdd95f9735aa7f6b784
xiezc <765150816@qq.com>
2022-06-25 16:17:44 +0800

    gradle 打包命令

--- vxpt-eurake/deploy.sh
+++ vxpt-eurake/deploy.sh
@@ -3,3 +3,12 @@ docker run --rm -u gradle \
 -v "$PWD/../":/home/gradle/project \
 -w /home/gradle/project/vxpt-eurake \
 gradle gradle assemble
+
+## 删除旧镜像
+docker rmi vxpt-eurake:1.0
+
+## 重新打包镜像
+docker build -t vxpt-eurake:1.0 .
+
+## 启动镜像
+docker run --rm -d -it --name eurake -p 9090:9090 vxpt-eurake:1.0
