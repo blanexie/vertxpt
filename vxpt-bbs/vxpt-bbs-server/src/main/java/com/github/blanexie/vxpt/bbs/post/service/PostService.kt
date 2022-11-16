5baf22279c9924e13f04d379c009d0d0aa1c42ba
xiezc <TLqodu25>
2022-11-16 19:56:21 +0800

    全局错误码

--- vxpt-bbs/vxpt-bbs-server/src/main/java/com/github/blanexie/vxpt/bbs/post/service/PostService.kt
+++ vxpt-bbs/vxpt-bbs-server/src/main/java/com/github/blanexie/vxpt/bbs/post/service/PostService.kt
@@ -7,6 +7,8 @@ import com.github.blanexie.vxpt.bbs.post.entity.LabelDO
 import com.github.blanexie.vxpt.bbs.post.entity.PostDO
 import com.github.blanexie.vxpt.bbs.post.repository.LabelRepository
 import com.github.blanexie.vxpt.bbs.post.repository.PostRepository
+import com.github.blanexie.vxpt.bbs.util.RespCode
+import com.github.blanexie.vxpt.bbs.util.VxptException
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -24,7 +26,7 @@ class PostService(val postRepository: PostRepository, val labelRepository: Label
     fun publish(id: Int, userId: Int) {
         val option = postRepository.findById(id)
         val postDO = option.orElseThrow {
-            Error("请传入正确的帖子id")
+            VxptException(RespCode.PARAM_ERROR, "请传入正确的帖子id")
         }
         postDO.publish()
         postRepository.save(postDO)
